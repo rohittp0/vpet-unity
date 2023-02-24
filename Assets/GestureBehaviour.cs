3350477418535d54898a3eccdc1f6595e7b7fe03
Rohit T P <tprohit9@gmail.com>
2023-02-24 15:58:17 +0530

    flick detection done

--- Assets/GestureBehaviour.cs
+++ Assets/GestureBehaviour.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class GestureBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float flickThreshold = 1f;
+    private Animator animator;
+    private Vector3 lastAcceleration;
+    
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
+        Vector3 acceleration = Input.acceleration;
+
+        // Calculate the difference in acceleration since the last frame
+        Vector3 deltaAcceleration = acceleration - lastAcceleration;
+
+        var x = Mathf.Abs(deltaAcceleration.x);
+        var y = Mathf.Abs(deltaAcceleration.y);
+        var z = Mathf.Abs(deltaAcceleration.z);
         
+        Debug.Log("x: " + x + " y: " + y + " z: " + z);
+
+        // Check if the difference in acceleration exceeds the flick threshold in any axis
+        if (Mathf.Abs(deltaAcceleration.x) > flickThreshold || 
+            Mathf.Abs(deltaAcceleration.y) > flickThreshold || 
+            Mathf.Abs(deltaAcceleration.z) > flickThreshold)
+        {
+            animator.SetTrigger("Fly");
+        }
+
+        lastAcceleration = acceleration;
     }
 }
